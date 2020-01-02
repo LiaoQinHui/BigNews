cbf3e01c09cfbb6f7f425b8e855e68f172c40361
Your Name <you@example.com>
2020-01-02 16:55:53 +0800

    WanSan

--- admin/js/http.js
+++ admin/js/http.js
@@ -14,7 +14,7 @@
   });
 
   //   url封装
-  const baseURL = "http://192.168.77.45:8080/api/v1";
+  const baseURL = "http://192.168.77.53:8080/api/v1";
   const urls = {
     //   获取用户信息
     userInfo: baseURL + "/admin/user/info",
